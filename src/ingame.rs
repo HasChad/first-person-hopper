0baa046cbfcabe2ad47ea21de39e7a5ab74b1f4c
HasChad <troll.yahya@gmail.com>
2023-09-11 00:34:50 +0300

    few bug fix and empty main menu for new ui

--- src/ingame.rs
+++ src/ingame.rs
@@ -1,11 +1,11 @@
 use bevy::{prelude::*, window::CursorGrabMode};
-use bevy_rapier2d::prelude::*;
 use bevy_cursor::prelude::*;
+use bevy_rapier2d::prelude::*;
 
-use crate::SCREEN_HEIGHT;
-use crate::SCREEN_WIDTH;
 use crate::AppState;
 use crate::GameDifficultyState;
+use crate::SCREEN_HEIGHT;
+use crate::SCREEN_WIDTH;
 
 #[derive(Component)]
 struct InGameEntity;
@@ -23,45 +23,39 @@ pub struct InGamePlugin;
 
 impl Plugin for InGamePlugin {
     fn build(&self, app: &mut App) {
-        app
-            .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1.0))
+        app.add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1.0))
             // ! .add_plugins(RapierDebugRenderPlugin::default())
             .add_plugins(CursorInfoPlugin)
-            .add_systems(OnEnter(AppState::InGame), (
-                setup,
-                game_diffuculty_easy.run_if(in_state(GameDifficultyState::Easy)),
-                game_diffuculty_hard.run_if(in_state(GameDifficultyState::Hard)),
-            ))
-
-            .add_systems(Update, (
-                cursor_position,
-                ball_movement,
-                entity_despawner
-            ).run_if(in_state(AppState::InGame)));   
+            .add_systems(
+                OnEnter(AppState::InGame),
+                (
+                    setup,
+                    game_diffuculty_easy.run_if(in_state(GameDifficultyState::Easy)),
+                    game_diffuculty_hard.run_if(in_state(GameDifficultyState::Hard)),
+                ),
+            )
+            .add_systems(
+                Update,
+                (cursor_position, ball_movement, entity_despawner)
+                    .run_if(in_state(AppState::InGame)),
+            );
     }
 }
 
-fn game_diffuculty_hard(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>
-) {
-
-}
-
-fn game_diffuculty_easy(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>
-) {
-    info!("normal ball created");
+fn game_diffuculty_hard(mut commands: Commands, asset_server: Res<AssetServer>) {
+    info!("Hard ball created");
 
     //normal jump-ball spawn
-    commands.spawn(
-        SpriteBundle{
+    commands
+        .spawn(SpriteBundle {
             texture: asset_server.load("sprites\\ball.png"),
             ..default()
         })
         .insert(Collider::ball(50.0))
-        .insert(Sleeping {sleeping: true, ..default()})
+        .insert(Sleeping {
+            sleeping: true,
+            ..default()
+        })
         .insert(TransformBundle::from(Transform::from_xyz(0.0, 0.0, -1.0)))
         .insert(RigidBody::Dynamic)
         .insert(GravityScale(17.0))
@@ -82,12 +76,41 @@ fn game_diffuculty_easy(
         .insert(InGameEntity);
 }
 
+fn game_diffuculty_easy(mut commands: Commands, asset_server: Res<AssetServer>) {
+    info!("Normal ball created");
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut windows: Query<&mut Window>,
-) {
+    //normal jump-ball spawn
+    commands
+        .spawn(SpriteBundle {
+            texture: asset_server.load("sprites\\ball.png"),
+            ..default()
+        })
+        .insert(Collider::ball(50.0))
+        .insert(Sleeping {
+            sleeping: true,
+            ..default()
+        })
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 0.0, -1.0)))
+        .insert(RigidBody::Dynamic)
+        .insert(GravityScale(17.0))
+        .insert(ColliderMassProperties::Density(0.1))
+        .insert(Restitution {
+            coefficient: 1.0,
+            combine_rule: CoefficientCombineRule::Average,
+        })
+        .insert(Velocity {
+            linvel: Vec2::new(0.0, 0.0),
+            angvel: 0.0,
+        })
+        .insert(ExternalImpulse {
+            impulse: Vec2::new(0.0, 0.0),
+            torque_impulse: 0.0,
+        })
+        .insert(Ball)
+        .insert(InGameEntity);
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut windows: Query<&mut Window>) {
     info!("Game Started");
 
     //lock and hide crosshair
@@ -96,18 +119,18 @@ fn setup(
     window.cursor.grab_mode = CursorGrabMode::Confined;
 
     //spawn m4
-    commands.spawn(
-        SpriteBundle{
+    commands
+        .spawn(SpriteBundle {
             texture: asset_server.load("sprites\\m4.png"),
             ..default()
         })
         .insert(TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)))
         .insert(M4)
         .insert(InGameEntity);
- 
+
     //crosshair and collision spawn
-    commands.spawn(
-        SpriteBundle{
+    commands
+        .spawn(SpriteBundle {
             texture: asset_server.load("sprites\\crosshair.png"),
             ..default()
         })
@@ -120,8 +143,8 @@ fn setup(
 
     //spawn side walls
     //right wall
-    commands.spawn(
-        SpriteBundle{
+    commands
+        .spawn(SpriteBundle {
             texture: asset_server.load("sprites\\wall.png"),
             sprite: Sprite {
                 flip_x: true,
@@ -129,22 +152,30 @@ fn setup(
             },
             ..default()
         })
-        .insert(Collider::cuboid(100.0, SCREEN_HEIGHT/2.0 + 500.0))
-        .insert(TransformBundle::from(Transform::from_xyz(SCREEN_WIDTH/2.0, 0.0, -1.0)))
+        .insert(Collider::cuboid(100.0, SCREEN_HEIGHT / 2.0 + 500.0))
+        .insert(TransformBundle::from(Transform::from_xyz(
+            SCREEN_WIDTH / 2.0,
+            0.0,
+            -1.0,
+        )))
         .insert(Friction {
             coefficient: 0.0,
             combine_rule: CoefficientCombineRule::Min,
         })
         .insert(InGameEntity);
-        
+
     //left wall
-    commands.spawn(
-        SpriteBundle{
+    commands
+        .spawn(SpriteBundle {
             texture: asset_server.load("sprites\\wall.png"),
             ..default()
         })
-        .insert(Collider::cuboid(100.0, SCREEN_HEIGHT/2.0 + 500.0))
-        .insert(TransformBundle::from(Transform::from_xyz(-SCREEN_WIDTH/2.0, 0.0, -1.0)))
+        .insert(Collider::cuboid(100.0, SCREEN_HEIGHT / 2.0 + 500.0))
+        .insert(TransformBundle::from(Transform::from_xyz(
+            -SCREEN_WIDTH / 2.0,
+            0.0,
+            -1.0,
+        )))
         .insert(Friction {
             coefficient: 0.0,
             combine_rule: CoefficientCombineRule::Min,
@@ -155,10 +186,10 @@ fn setup(
 fn cursor_position(
     q_windows: Res<CursorInfo>,
     mut crosshair: Query<&mut Transform, With<CursorCrosshair>>,
-    mut m4: Query<&mut Transform, (With<M4>, Without<CursorCrosshair>)>
+    mut m4: Query<&mut Transform, (With<M4>, Without<CursorCrosshair>)>,
 ) {
     let Some(position) = q_windows.position() else {
-        return () //info!("out of window");
+        return; //info!("out of window");
     };
 
     let mut m4_position = m4.single_mut();
@@ -180,12 +211,11 @@ fn ball_movement(
     input: Res<Input<MouseButton>>,
     rapier_context: Res<RapierContext>,
     asset_server: Res<AssetServer>,
-) { 
+) {
     let entity_ball = ball.single().0;
     let entity_cross = crosshair.single();
 
     if input.just_pressed(MouseButton::Left) {
-
         //sound play
         commands.spawn(AudioBundle {
             source: asset_server.load("sounds\\M16.ogg"),
@@ -193,7 +223,7 @@ fn ball_movement(
         });
 
         //jump ball if collide eachother
-        for (_, mut ball_impulse, mut ball_velocity) in &mut ball{
+        for (_, mut ball_impulse, mut ball_velocity) in &mut ball {
             if rapier_context.intersection_pair(entity_ball, entity_cross) == Some(true) {
                 ball_velocity.linvel.y = 0.0;
                 ball_velocity.linvel.x = 0.0;
@@ -201,12 +231,12 @@ fn ball_movement(
                 ball_impulse.impulse.y = alea::f32_in_range(500000.0, 900000.0);
                 ball_impulse.impulse.x = alea::f32_in_range(-500000.0, 500000.0);
                 ball_impulse.torque_impulse = alea::f32_in_range(-10000000.0, 10000000.0);
-            } 
+            }
         }
     }
 }
 
-fn entity_despawner (
+fn entity_despawner(
     mut entities: Query<Entity, With<InGameEntity>>,
     mut commands: Commands,
     ball: Query<&Transform, With<Ball>>,
@@ -214,7 +244,7 @@ fn entity_despawner (
 ) {
     if ball.single().translation.y < -420.0 {
         info!("Despawner Activated");
-        
+
         //enable cursor
         let mut window = windows.single_mut();
         window.cursor.visible = true;
@@ -222,11 +252,10 @@ fn entity_despawner (
 
         //change state
         commands.insert_resource(NextState(Some(AppState::MainMenu)));
-        
+
         //despawn everyting in InGame
         for entities_despawner in &mut entities {
             commands.entity(entities_despawner).despawn();
         }
     }
 }
-
